80c3db81e9de866e0ec234ba645207d1267cabd7
Denis Listiadi <denislistiadi24@gmail.com>
2025-04-21 16:06:07 +0700

    feat: implement ApiError class for structured error handling

---
+++ src/utils/apiError.ts
@@ -0,0 +1,18 @@
+class ApiError extends Error {
+  public isOperational: boolean;
+  public stack?: string;
+  public statusCode: number;
+
+  constructor(statusCode: number, message: string, isOperational = true, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    if (stack) {
+      this.stack = process.env.NODE_ENV === 'development' ? stack : undefined;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
+export default ApiError;
